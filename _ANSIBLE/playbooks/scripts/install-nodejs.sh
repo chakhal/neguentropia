8c7197bcea6665bfc83af6e157a98b5f8075185a
Cyril <cyril.fabby@gmail.com>
2017-01-21 13:05:20 +0100

    Install nodejs

---
+++ _ANSIBLE/playbooks/scripts/install-nodejs.sh
@@ -0,0 +1,15 @@
+# To use a newer version, for example 6.3.1, use instead:
+NODE_VERSION=$1
+
+cd /opt/download
+
+curl -LO http://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.gz
+tar xzf node-$NODE_VERSION-linux-x64.tar.gz
+cp -rp node-$NODE_VERSION-linux-x64 /usr/local/
+
+ln -s /usr/local/node-$NODE_VERSION-linux-x64 /usr/local/node
+ln -s /usr/local/node-$NODE_VERSION-linux-x64/bin/node /usr/local/bin/node
+ln -s /usr/local/node-$NODE_VERSION-linux-x64/bin/npm /usr/local/bin/npm
+
+
+touch /opt/provisioned/nodejs-${NODE_VERSION}
